9ed0fa901e25e3b7efd625733f26152bc482d3e6
Daniel-johnstone <daniel.johnstone.4.19@gmail.com>
2021-03-05 07:33:38 +0000

    final task

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
 def bowel():
-   print("Keep it logically awesome.")
 
    f = open("quotes.txt")
    quotes = f.readlines()
+   #f.write("\n crazy stuff")
    f.close()
 
-   print(quotes[13])
+   print(quotes[4])
+   print('\n')
+   print(quotes[14])
 
 if __name__== "__main__":
   bowel()
